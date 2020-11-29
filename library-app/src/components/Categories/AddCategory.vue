f4b137e25d29c75031cc5adb16adf3b50d9e55fd
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 16:00:39 +0100

    added form

---
+++ library-app/src/components/Categories/AddCategory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <form @submit.prevent="$emit(addCategory)">
+      <label for="categoryName">Add a new Category</label>
+      <input
+        type="text"
+        id="categoryName"
+        placeholder="Add a unique Category name..."
+        v-model="categoryName"
+      />
+      <button class="btn">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddCategory',
+  data() {
+    return {
+      categoryName: '',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
