43058c9aa75aa7a9e9a56862c5c6d75ec4125f84
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 23:27:44 +0100

    init file

---
+++ library-app/src/components/LibraryItems/LibraryNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="">
+    <li class="nav-item">
+      <router-link :to="{ name: 'Home' }" exact class="nav-link">
+        Add Library Item
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{ name: 'Categories' }" exact class="nav-link">
+        Edit Library Item
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{ name: 'LibraryItems' }" exact class="nav-link">
+        Check in/Check out Library item
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
