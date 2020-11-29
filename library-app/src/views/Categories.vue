cc89f455ca4680cdf7a313bc7295472a6f57cf4b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 16:00:56 +0100

    created Categories page

---
+++ library-app/src/views/Categories.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Categories</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Categories',
+  components: {},
+  data() {
+    return {
+      categories: [
+        { id: 1, categoryName: 'Fiction' },
+        { id: 2, categoryName: 'Biography' },
+        { id: 3, categoryName: 'Romance' },
+        { id: 4, categoryName: 'Thriller' },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
