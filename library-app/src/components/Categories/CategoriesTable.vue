8e7c729bb03b0a6f2788e62241785b6fbfbbddc2
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 18:17:07 +0100

    added catergories table

---
+++ library-app/src/components/Categories/CategoriesTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="categories.length">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Number of Library Items</th>
+          <th scope="col">Remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        <categoryItem
+          v-for="_category in categories"
+          :key="_category.id"
+          :category="_category"
+        />
+      </tbody>
+    </table>
+  </div>
+  <div v-else>
+    <h1>No categories added</h1>
+  </div>
+</template>
+
+<script>
+import CategoryItem from './CategoryItem';
+
+export default {
+  name: 'CategoriesTable',
+  props: ['categories'],
+  components: {
+    CategoryItem,
+  },
+};
+</script>
+
+<style>
+</style>
