ce27462f94c817990500183a34db2d1b28bde4c4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 22:58:53 +0100

    updated form

--- library-app/src/components/Categories/AddCategory.vue
+++ library-app/src/components/Categories/AddCategory.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    <form @submit.prevent="$emit('add-category', categoryName)">
-      <label for="categoryName">Add a new Category</label>
-      <input
-        type="text"
-        id="categoryName"
-        placeholder="Add a unique Category name..."
-        v-model="categoryName"
-      />
-      <button class="btn">Add</button>
-    </form>
-  </div>
+  <form @submit.prevent="$emit('add-category', categoryName)">
+    <div class="form-row text-left">
+      <label for="categoryName">Category name:</label>
+      <div class="input-group">
+        <input
+          class="form-control"
+          type="text"
+          id="categoryName"
+          v-model="categoryName"
+        />
+        <div class="input-group-append">
+          <button class="btn btn-dark">Add</button>
+        </div>
+      </div>
+      <div class="input-group">
+        <div class="invalid-feedback">Please insert valid input!</div>
+        <small class="form-text text-muted"
+          >The Category name must be atleast 3 characters long and has to be
+          unique.</small
+        >
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
