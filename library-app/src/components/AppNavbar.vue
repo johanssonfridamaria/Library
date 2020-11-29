2c3857c0f5f1bf8d88fd2027f237eebc1c48df8d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 16:00:15 +0100

    init navbar and added router-links

---
+++ library-app/src/components/AppNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link :to="{ name: 'Home' }" exact> Home </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Categories' }" exact>
+          Categories
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'LibraryItems' }" exact>
+          Library Items
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Employees' }" exact> Employees </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+};
+</script>
+
+<style>
+.router-link-active {
+  color: green;
+}
+</style>
