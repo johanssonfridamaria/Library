119d1265a0321ede0eff0e072113d7c9a42ddbcb
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 18:17:22 +0100

    added category item

---
+++ library-app/src/components/Categories/CategoryItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <th scope="row">{{ category.id }}</th>
+    <td>{{ category.categoryName }}</td>
+    <td>1/4</td>
+    <td><button>Remove</button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'CategoryItem',
+  props: ['category'],
+};
+</script>
+
+<style>
+</style>
