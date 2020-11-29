3e72fe0241da0bfd0248965249243f0a89ece969
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 22:59:03 +0100

    added styling

--- library-app/src/components/Categories/CategoriesTable.vue
+++ library-app/src/components/Categories/CategoriesTable.vue
@@ -1,25 +1,27 @@
 <template>
-  <div v-if="categories.length">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Name</th>
-          <th scope="col">Number of Library Items</th>
-          <th scope="col">Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        <categoryItem
-          v-for="_category in categories"
-          :key="_category.id"
-          :category="_category"
-        />
-      </tbody>
-    </table>
-  </div>
-  <div v-else>
-    <h1>No categories added</h1>
+  <div class="table-responsive">
+    <div v-if="categories.length">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Number of Library Items</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          <categoryItem
+            v-for="_category in categories"
+            :key="_category.id"
+            :category="_category"
+          />
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <h1>No categories added</h1>
+    </div>
   </div>
 </template>
 
