f6b98632dd9c75bcd39746da131edcf3d842d9d4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 23:27:58 +0100

    added content to library page

--- library-app/src/views/LibraryItems.vue
+++ library-app/src/views/LibraryItems.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>LibraryItems</h1>
+  <div class="container">
+    <h1 class="py-4 border-bottom text-left">Library Items</h1>
+    <div class="py-5 mb-4 col-md-8">
+      <h2 class="text-left mb-4">Manage Library Items</h2>
+      <addCategory @add-category="add" :categories="categories" />
+    </div>
+    <div class="col-12">
+      <h2 class="text-left mb-4">Library</h2>
+      <categoriesTable :categories="categories" />
+    </div>
   </div>
 </template>
 
