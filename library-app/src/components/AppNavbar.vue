fda3abb77d96ad5af5f2c2b4e31460737c74adf2
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 18:58:30 +0100

    added styling

--- library-app/src/components/AppNavbar.vue
+++ library-app/src/components/AppNavbar.vue
@@ -1,23 +1,41 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link :to="{ name: 'Home' }" exact> Home </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'Categories' }" exact>
-          Categories
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'LibraryItems' }" exact>
-          Library Items
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'Employees' }" exact> Employees </router-link>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Library</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link :to="{ name: 'Home' }" exact class="nav-link">
+            Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Categories' }" exact class="nav-link">
+            Categories
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'LibraryItems' }" exact class="nav-link">
+            Library Items
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Employees' }" exact class="nav-link">
+            Employees
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
