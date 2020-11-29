ab72716abf7da25c2a69a82395ecc7f428b15f67
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-29 16:01:15 +0100

    initialised files

---
+++ library-app/src/views/Employees.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Employees</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Employees',
+  components: {},
+};
+</script>
+
+<style></style>
---
+++ library-app/src/views/LibraryItems.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>LibraryItems</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LibraryItems',
+  components: {},
+};
+</script>
+
+<style></style>
