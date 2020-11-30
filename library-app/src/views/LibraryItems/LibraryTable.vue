6ae4ab9d610f394cc1f28a20355163e5ae124db3
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 13:46:10 +0100

    added table

--- library-app/src/views/LibraryItems/LibraryTable.vue
+++ library-app/src/views/LibraryItems/LibraryTable.vue
@@ -1,9 +1,64 @@
 <template>
-  <div>Library Table</div>
+  <div class="table-responsive">
+    <div v-if="library.length">
+      <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Category Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Pages</th>
+            <th scope="col">Run Time (min)</th>
+            <th scope="col">Borrowable</th>
+            <th scope="col">Borrower</th>
+            <th scope="col">Borrow Date</th>
+            <th scope="col">Type</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          <libraryItem
+            v-for="item in library"
+            :key="item.id"
+            :libraryItem="item"
+          />
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <h1>No Library items added</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import LibraryItem from '../../components/LibraryItems/LibraryItem';
+
+export default {
+  name: 'LibraryTable',
+  components: {
+    LibraryItem,
+  },
+  data() {
+    return {
+      library: [
+        {
+          id: 1,
+          categoryId: 5,
+          title: 'Cats and dogs',
+          author: 'hej',
+          pages: 234,
+          runTime: 120,
+          borrowable: true,
+          borrower: 'Frida Johansson',
+          borrowDate: Date.now(),
+          type: 'book',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
