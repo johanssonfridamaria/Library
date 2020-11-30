9741e08cd6d97b028671407ce11ae8e0b0ecb15e
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 09:56:24 +0100

    changed styling

--- library-app/src/views/LibraryItems.vue
+++ library-app/src/views/LibraryItems.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="container">
-    <h1 class="py-4 border-bottom text-left">Library Items</h1>
-    <div class="py-5 mb-4 col-md-8">
-      <h2 class="text-left mb-4">Manage Library Items</h2>
-      <addCategory @add-category="add" :categories="categories" />
-    </div>
-    <div class="col-12">
-      <h2 class="text-left mb-4">Library</h2>
-      <categoriesTable :categories="categories" />
+    <div class="row">
+      <div class="col-3">
+        <libraryNav />
+      </div>
+      <div class="col-9">
+        <router-view />
+        <h2 class="text-left mb-4">Library</h2>
+        <libraryTable />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import LibraryNav from '../components/LibraryItems/LibraryNav';
+import LibraryTable from '../components/LibraryItems/LibraryTable';
+
 export default {
   name: 'LibraryItems',
-  components: {},
+  components: {
+    LibraryNav,
+    LibraryTable,
+    // data() {
+    //   return {
+    //     libraryItems: [],
+    //   };
+    // },
+  },
 };
 </script>
 
