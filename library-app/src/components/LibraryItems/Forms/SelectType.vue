a06e591b0e6b8cac516fb3c5e1f3186481fef4cf
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 15:27:58 +0100

    added change function

--- library-app/src/components/LibraryItems/Forms/SelectType.vue
+++ library-app/src/components/LibraryItems/Forms/SelectType.vue
@@ -1,19 +1,16 @@
 <template>
-  <form @submit.prevent="$emit('select-type', select)">
-    <div class="form-row text-left">
-      <label for="selectType"
-        >Please select what type of Item you want to add</label
-      >
-      <div class="input-group">
-        <select class="form-control" v-model="select" id="selectType">
-          <option value="book">Book</option>
-          <option value="dvd">DVD</option>
-          <option value="audio-book">Audio Book</option>
-          <option value="ref-book">Reference Book</option>
-        </select>
-      </div>
-    </div>
-  </form>
+  <div class="input-group">
+    <select
+      class="form-control"
+      v-model="select"
+      @change="$emit('select-type', select)"
+    >
+      <option value="book">Book</option>
+      <option value="dvd">DVD</option>
+      <option value="audio-book">Audio Book</option>
+      <option value="ref-book">Reference Book</option>
+    </select>
+  </div>
 </template>
 
 <script>
