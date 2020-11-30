a7d109b9c92c829d73e6fb57b48f654f949f5a07
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 21:55:16 +0100

    created libraryItemSchema

---
+++ library-api/models/libraryItems/libraryItemSchema.js
@@ -0,0 +1,25 @@
+//imports mongoose from node modules
+const mongodb = require('mongoose');
+const Category = require('./categorySchema');
+
+//creating mongodb Schema
+const libraryItemSchema = mongodb.Schema({
+
+  _id: mongodb.Schema.Types.ObjectId,
+  categoryId: { type: Schema.Types.ObjectId, ref: Category, required:true },
+  title: { type: String, required: true},
+  author: { type: String, required: true},
+  pages: { type: Number, required: true},
+  runTimeMinutes: { type: String, required: true},
+  isBorrowable: { type: Boolean, required: true},
+  borrower: { type: String },
+  borrowDate: { type: Date, default: Date.now() },
+  type: { type: String },
+  
+  created: { type: Date, default: Date.now() },
+  modified: { type: Date, default: Date.now() },
+
+});
+
+//export file
+module.exports = mongodb.model('LibraryItem', libraryItemSchema);
