bd99a22076aa28d6efb292b9c5ef2b3f6aa5d7bd
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 22:20:51 +0100

    added routes

--- library-api/controllers/libraryItemsController.js
+++ library-api/controllers/libraryItemsController.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
 const LibraryItemModel = require('../models/libraryItems/libraryItemModel');
 
+//get library items
+router.get('/', LibraryItemModel.getLibraryItems);
+//get one library item
+router.get('/:id', LibraryItemModel.getOneLibraryItem);
+//create a new libraryitem
+router.post('/new', LibraryItemModel.createLibraryItem);
+//update specific library item
+router.patch('/:id', LibraryItemModel.updateLibraryItem);
+//check out library item
+router.patch('/:id', LibraryItemModel.checkOutLibraryItem);
+// check in library item
+router.patch('/:id', LibraryItemModel.checkInLibraryItem);
+//delete specific library item
+router.delete('/:id', LibraryItemModel.deleteLibraryItem);
+
+//export router
+module.exports = router;
