b1d79b9129391d5d9c23764ed455b5bc61f24be4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 15:28:17 +0100

    changed styling

--- library-app/src/views/LibraryItems.vue
+++ library-app/src/views/LibraryItems.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="py-4 border-bottom text-left">Library Items</h1>
     <div class="py-5 row">
-      <div class="col-md-3">
+      <div class="col-md-3 bg-light">
         <libraryTabs />
       </div>
       <div class="col-md-9">
