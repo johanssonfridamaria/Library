5e6bc85c348f3d358a58907e89666504664f3ebf
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 13:45:50 +0100

    added table

---
+++ library-app/src/components/LibraryItems/LibraryItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <tr class="align-items-center">
+    <th scope="row">{{ libraryItem.id }}</th>
+    <td>{{ libraryItem.title }}</td>
+    <td>{{ libraryItem.categoryId }}</td>
+    <td>{{ libraryItem.author }}</td>
+    <td>{{ libraryItem.pages }}</td>
+    <td>{{ libraryItem.runTime }}</td>
+    <td>{{ libraryItem.borrowable }}</td>
+    <td>{{ libraryItem.borrower }}</td>
+    <td>{{ libraryItem.borrowDate }}</td>
+    <td>{{ libraryItem.type }}</td>
+    <td><button class="btn btn-danger">Remove</button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'LibraryItem',
+  props: ['libraryItem'],
+};
+</script>
+
+<style></style>
