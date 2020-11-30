bb4702b19c5c00c2c15a089f9aae2165d25a4da5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 09:57:29 +0100

    renamed

---
+++ library-app/src/components/LibraryItems/LibraryTabs.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <router-link :to="{ name: 'AddItem' }" exact class="nav-link">
+        Add Library Item
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{ name: 'EditItem' }" exact class="nav-link">
+        Edit Library Item
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{ name: 'LendItem' }" exact class="nav-link">
+        Check in/Check out Library item
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
