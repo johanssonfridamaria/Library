034a6029f1e993ab8c7ca82caa0ba58941647695
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 21:21:07 +0100

    created categorymodel

---
+++ library-api/models/categories/categoryModel.js
@@ -0,0 +1,105 @@
+//imports mongoose from node modules
+const mongodb = require('mongoose');
+
+const Category = require('./categorySchema');
+
+//get all categories in Categories
+exports.getCategories = (req, res) => {
+  Category.find()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({
+      statusCode: 500,
+      status: false,
+      message: 'Failed to get categories',
+      error: err
+    }));
+};
+
+//get one category
+exports.getOneCategory = (req, res) => {
+  Category.findById(req.params.id)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({
+      statusCode: 500,
+      status: false,
+      message: 'Failed to get category',
+      error: err
+    }));
+};
+
+//create a new category
+exports.createCategory = (req, res) => {
+  Category.find({ name: req.body.name })
+    .then(exists => {
+      if (exists.length > 0) {
+        return res.status(400).json({
+          statusCode: 400,
+          status: false,
+          message: 'A category by that name already exists, please update category instead'
+        });
+      };
+
+      const category = new Category({
+        _id: new mongodb.Types.ObjectId,
+        name: req.body.name,
+      });
+
+      category.save()
+        .then(() => {
+          res.status(201).json({
+            statusCode: 201,
+            status: true,
+            message: 'Category created'
+          });
+        })
+        .catch(() => {
+          res.status(500).json({
+            statusCode: 500,
+            status: false,
+            message: 'Failed to create category'
+          });
+        });
+    });
+};
+
+//update category
+exports.updateCategory = (req, res) => {
+  Category.updateOne({ _id: req.params.id }, req.body)
+  .then(() => {
+      Category.updateOne({ _id: req.params.id }, { $set: { modified: Date.now()}})
+      .then(() => {
+          res.status(200).json({
+              statusCode: 200,
+              status: true,
+              message: 'Category updated'
+          })
+      })
+  })
+  .catch(() => {
+      res.status(500).json({
+          statusCode: 500,
+          status: false,
+          message: 'Failed to update category'
+      })
+  })
+}
+
+//delete category
+exports.deleteCategory = (req, res) => {
+  Category.deleteOne({ _id: req.params.id })
+  .then(() => {
+      res.status(200).json({
+          statusCode: 200,
+          status: true,
+          message: 'Category deleted!'
+      })
+  })
+  .catch(() => {
+      res.status(500).json({
+          statusCode: 500,
+          status: false,
+          message: 'Failed to delete category'
+      })
+  })
+}
+
