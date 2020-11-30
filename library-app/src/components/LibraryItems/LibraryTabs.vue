7df3916e3a421ce4dc5aa0ca5bed3df680652ec1
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-30 11:10:12 +0100

    added styling

--- library-app/src/components/LibraryItems/LibraryTabs.vue
+++ library-app/src/components/LibraryItems/LibraryTabs.vue
@@ -1,26 +1,49 @@
 <template>
-  <ul class="nav flex-column">
-    <li class="nav-item">
-      <router-link :to="{ name: 'AddItem' }" exact class="nav-link">
-        Add Library Item
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link :to="{ name: 'EditItem' }" exact class="nav-link">
-        Edit Library Item
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link :to="{ name: 'LendItem' }" exact class="nav-link">
-        Check in/Check out Library item
-      </router-link>
-    </li>
-  </ul>
+  <div class="navbar navbar-light bg-light">
+    <ul class="nav flex-column navbar-nav text-left">
+      <li class="nav-item">
+        <router-link :to="{ name: 'LibraryTable' }" exact class="nav-link">
+          Library Items
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'AddItem' }" exact class="nav-link">
+          Add Library Item
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'EditItem' }" exact class="nav-link">
+          Edit Library Item
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'LendItem' }" exact class="nav-link">
+          Check in/Check out Library item
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'LibraryTabs',
+  data() {
+    return {
+      tabs: [
+        'Library Items',
+        'Add Library Item',
+        'Edit Library Item',
+        'Check in/Check out Library Item',
+      ],
+      selectedTab: 'Library Items',
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.router-link-active {
+  color: white;
+}
 </style>
