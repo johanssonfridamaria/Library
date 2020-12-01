184f742679fd458161344c9c3c1d157301d482ed
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-01 13:00:52 +0100

    added no of library itemd

--- library-api/models/categories/categorySchema.js
+++ library-api/models/categories/categorySchema.js
@@ -1,11 +1,14 @@
 //imports mongoose from node modules
 const mongodb = require('mongoose');
+const LibraryItem = require('../libraryItems/libraryItemSchema');
+const ObjectId = mongodb.Schema.Types.ObjectId
 
 //creating mongodb Schema
 const categorySchema = mongodb.Schema({
 
-  _id: mongodb.Schema.Types.ObjectId,
-  name: { type: String, required: true, unique: true },
+  _id: ObjectId,
+  name: { type: String, required: true, unique: true, collation: { locale: 'en', strength: 2 } },
+  numberOfLibraryItems: {type: Number, default: ''},
 
   created: { type: Date, default: Date.now() },
   modified: { type: Date, default: Date.now() },
--- library-api/models/libraryItems/libraryItemSchema.js
+++ library-api/models/libraryItems/libraryItemSchema.js
@@ -1,12 +1,13 @@
 //imports mongoose from node modules
 const mongodb = require('mongoose');
 const Category = require('../categories/categorySchema');
+const ObjectId = mongodb.Schema.Types.ObjectId
 
 //creating mongodb Schema
 const libraryItemSchema = mongodb.Schema({
 
   _id: mongodb.Schema.Types.ObjectId,
-  categoryId: { type: mongodb.Schema.Types.ObjectId, ref: Category, required:true },
+  categoryId: { type: ObjectId, ref: 'Category'},
   title: { type: String, required: true},
   author: { type: String, required: true},
   pages: { type: Number, required: true},
