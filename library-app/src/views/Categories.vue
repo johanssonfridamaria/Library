0bc76c198bd07bdb0e86ee5e5c3a7e316913a4ba
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-01 18:24:17 +0100

    added edit component

--- library-app/src/views/Categories.vue
+++ library-app/src/views/Categories.vue
@@ -3,7 +3,19 @@
     <h1 class="py-4 border-bottom text-left">Categories</h1>
     <div class="py-5 mb-4 col-md-6">
       <h2 class="text-left mb-4">Add a new Category</h2>
-      <addCategory @add-category="addCategory" :categories="categories" />
+      <addCategory
+        @add-category="addCategory"
+        :categories="categories"
+        :errorMessage="errorMessage"
+      />
+    </div>
+    <div class="mb-4 col-md-6">
+      <h2 class="text-left mb-4">Edit Category</h2>
+      <editCategory
+        @update-category="updateCategory"
+        :categories="categories"
+        :errorMessage="errorMessage"
+      />
     </div>
     <div class="col-12">
       <h2 class="text-left mb-4">Added categories</h2>
@@ -18,17 +30,20 @@
 <script>
 import AddCategory from '../components/Categories/AddCategory';
 import CategoriesTable from '../components/Categories/CategoriesTable';
+import EditCategory from '../components/Categories/EditCategory';
 
 export default {
   name: 'Categories',
   components: {
     AddCategory,
     CategoriesTable,
+    EditCategory,
   },
   data() {
     return {
       categories: [],
       apiURI: 'http://localhost:8000/api/categories',
+      errorMessage: '',
     };
   },
   methods: {
@@ -36,11 +51,9 @@ export default {
     fetchCategories() {
       fetch(this.apiURI)
         .then(response => response.json())
-        // .then(this.handleErrors)
         .then(categoriesInDb => {
           this.categories = categoriesInDb;
         });
-      // .catch(err => console.log(err));
     },
     addCategory(input) {
       fetch(this.apiURI + '/new', {
@@ -53,8 +66,23 @@ export default {
         }),
       })
         .then(response => response.json())
+        .then(this.handleErrors)
         .then(() => this.fetchCategories());
     },
+    // updateCategory(id) {
+    //   fetch(this.apiURI + `${id}`, {
+    //     method: 'PATCH',
+    //     headers: {
+    //       'Content-type': 'application/json; charset=UTF-8',
+    //     },
+    //     body: JSON.stringify({
+    //       name: id.name,
+    //     }),
+    //   })
+    //     .then(response => response.json())
+    //     .then(this.handleErrors)
+    //     .then(() => this.fetchCategories());
+    // },
     delCategory(id) {
       fetch(this.apiURI + `/${id}`, {
         method: 'DELETE',
@@ -62,10 +90,12 @@ export default {
         .then(response => response.json())
         .then(() => this.fetchCategories());
     },
-    // handleErrors(response) {
-    //   if (!response.ok) throw new Error(response.error);
-    //   return response;
-    // },
+    handleErrors(res) {
+      if (res.statusCode !== 201) {
+        this.errorMessage = res.message;
+      }
+      return res;
+    },
   },
   //Fetches categories from DB when app starts
   created() {
