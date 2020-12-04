ef0831db887a81cad1004706a303737730e94c62
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-04 16:02:50 +0100

    removed unessecary routes

--- library-api/controllers/libraryItemsController.js
+++ library-api/controllers/libraryItemsController.js
@@ -3,16 +3,16 @@ const LibraryItemModel = require('../models/libraryItems/libraryItemModel');
 
 //get library items
 router.get('/', LibraryItemModel.getLibraryItems);
+
 //get one library item
 router.get('/:id', LibraryItemModel.getOneLibraryItem);
+
 //create a new libraryitem
 router.post('/new', LibraryItemModel.createLibraryItem);
+
 //update specific library item
 router.patch('/:id', LibraryItemModel.updateLibraryItem);
-//lend library item
-// router.patch('/:id', LibraryItemModel.lendLibraryItem);
-// // check in library item
-// router.patch('/:id', LibraryItemModel.checkInLibraryItem);
+
 //delete specific library item
 router.delete('/:id', LibraryItemModel.deleteLibraryItem);
 
